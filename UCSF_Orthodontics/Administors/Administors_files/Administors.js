0665cda86f29bd07cee160876d63c1bffd487fea
Kjeld Aamodt <Kjeld.aamodt@gmail.com>
2016-05-11 13:06:42 -0700

    updated for potential applicants

--- UCSF_Orthodontics/Administors/Administors_files/Administors.js
+++ UCSF_Orthodontics/Administors/Administors_files/Administors.js
@@ -6,6 +6,6 @@ function photocastSubscribe()
 {photocastHelper("http://carmelartist.com/ucsfortho/UCSF_Orthodontics/Administors/rss.xml");}
 function onPageLoad()
 {loadMozillaCSS('Administors_files/AdministorsMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');detectBrowser();adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');Widget.onload();fixAllIEPNGs('../Media/transparent.gif');fixupIECSS3Opacity('id3');performPostEffectsFixups()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');detectBrowser();adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('../Media/transparent.gif');performPostEffectsFixups()}
 function onPageUnload()
 {Widget.onunload();}
