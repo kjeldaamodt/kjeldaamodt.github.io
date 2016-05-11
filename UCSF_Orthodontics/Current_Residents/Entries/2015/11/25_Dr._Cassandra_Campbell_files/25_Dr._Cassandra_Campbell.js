ffd298a64d5e41b5baa7094e58581d91678d7b99
Kjeld Aamodt <Kjeld.aamodt@gmail.com>
2016-05-10 20:27:24 -0700

    resident photos
    updated from brady and sara

--- UCSF_Orthodontics/Current_Residents/Entries/2015/11/25_Dr._Cassandra_Campbell_files/25_Dr._Cassandra_Campbell.js
+++ UCSF_Orthodontics/Current_Residents/Entries/2015/11/25_Dr._Cassandra_Campbell_files/25_Dr._Cassandra_Campbell.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20160203
+// Created by iWeb 3.0.4 local-build-20160510
 
 setTransparentGifURL('../../../../Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({reflection_0:new IWReflection({opacity:0.50,offset:1.00})});registry.applyEffects();}
@@ -6,6 +6,6 @@ function hostedOnDM()
 {return false;}
 function onPageLoad()
 {dynamicallyPopulate();loadMozillaCSS('25_Dr._Cassandra_Campbell_files/25_Dr._Cassandra_CampbellMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixAllIEPNGs('../../../../Media/transparent.gif');applyEffects()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixAllIEPNGs('../../../../Media/transparent.gif');BlogFixupPreviousNext();applyEffects()}
 function onPageUnload()
 {Widget.onunload();}
