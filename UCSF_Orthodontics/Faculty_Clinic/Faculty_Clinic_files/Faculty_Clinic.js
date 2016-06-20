3c4cf11d8f62af9dd4c416c46c2250f2fa2ff1f9
Kjeld Aamodt <Kjeld.aamodt@gmail.com>
2016-06-20 16:29:46 -0700

    added christian solem
    faculty practice addition

--- UCSF_Orthodontics/Faculty_Clinic/Faculty_Clinic_files/Faculty_Clinic.js
+++ UCSF_Orthodontics/Faculty_Clinic/Faculty_Clinic_files/Faculty_Clinic.js
@@ -1,13 +1,13 @@
-// Created by iWeb 3.0.4 local-build-20160517
+// Created by iWeb 3.0.4 local-build-20160620
 
 setTransparentGifURL('../Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke(),shadow_0:new IWShadow({blurRadius:10,offset:new IWPoint(4.2426,4.2426),color:'#000000',opacity:0.464033})});registry.applyEffects();}
+{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_0:new IWShadow({blurRadius:10,offset:new IWPoint(4.2426,4.2426),color:'#000000',opacity:0.464033}),stroke_0:new IWEmptyStroke()});registry.applyEffects();}
 function hostedOnDM()
 {return false;}
 function photocastSubscribe()
 {photocastHelper("http://carmelartist.com/ucsfortho/UCSF_Orthodontics/Faculty_Clinic/rss.xml");}
 function onPageLoad()
 {loadMozillaCSS('Faculty_Clinic_files/Faculty_ClinicMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');detectBrowser();adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');Widget.onload();fixAllIEPNGs('../Media/transparent.gif');applyEffects()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');detectBrowser();adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('../Media/transparent.gif');applyEffects()}
 function onPageUnload()
 {Widget.onunload();}
