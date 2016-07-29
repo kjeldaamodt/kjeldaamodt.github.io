01ae5b36bcbf5ff3f152955ca83f921e8be51887
Kjeld Aamodt <Kjeld.aamodt@gmail.com>
2016-07-29 09:15:36 -0700

    formatting dates

--- UCSF_Orthodontics/Current_Events/Entries/2016/7/29_Orthodontist_Fellowship_files/29_Orthodontist_Fellowship.js
+++ UCSF_Orthodontics/Current_Events/Entries/2016/7/29_Orthodontist_Fellowship_files/29_Orthodontist_Fellowship.js
@@ -3,7 +3,7 @@
 setTransparentGifURL('../../../../Media/transparent.gif');function hostedOnDM()
 {return false;}
 function onPageLoad()
-{dynamicallyPopulate();loadMozillaCSS('15_Prospective_International_Orthodontists_files/15_Prospective_International_OrthodontistsMoz.css')
+{dynamicallyPopulate();loadMozillaCSS('29_Orthodontist_Fellowship_files/29_Orthodontist_FellowshipMoz.css')
 adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');adjustLineHeightIfTooBig('id8');adjustFontSizeIfTooBig('id8');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('../../../../Media/transparent.gif');fixupIECSS3Opacity('id3');BlogFixupPreviousNext();performPostEffectsFixups()}
 function onPageUnload()
 {Widget.onunload();}
--- UCSF_Orthodontics/Current_Events/Entries/2016/7/29_Preceptorship_for_Dentists_files/29_Preceptorship_for_Dentists.js
+++ UCSF_Orthodontics/Current_Events/Entries/2016/7/29_Preceptorship_for_Dentists_files/29_Preceptorship_for_Dentists.js
@@ -3,7 +3,7 @@
 setTransparentGifURL('../../../../Media/transparent.gif');function hostedOnDM()
 {return false;}
 function onPageLoad()
-{dynamicallyPopulate();loadMozillaCSS('15_International_Preceptorship_for_Dentists_files/15_International_Preceptorship_for_DentistsMoz.css')
+{dynamicallyPopulate();loadMozillaCSS('29_Preceptorship_for_Dentists_files/29_Preceptorship_for_DentistsMoz.css')
 adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');adjustLineHeightIfTooBig('id8');adjustFontSizeIfTooBig('id8');Widget.onload();fixAllIEPNGs('../../../../Media/transparent.gif');fixupIECSS3Opacity('id3');BlogFixupPreviousNext();performPostEffectsFixups()}
 function onPageUnload()
 {Widget.onunload();}
