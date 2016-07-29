34dcb9cfa7a0ca11f6dd39402d26db54eb6ad9de
Kjeld Aamodt <Kjeld.aamodt@gmail.com>
2016-07-29 09:12:50 -0700

    Update melissa fung
    adding/changing ib nielsen to melissa fung

--- UCSF_Orthodontics/Current_Events/Entries/2015/12/20_Orthodontic_Residency_files/20_Orthodontic_Residency.js
+++ UCSF_Orthodontics/Current_Events/Entries/2015/12/20_Orthodontic_Residency_files/20_Orthodontic_Residency.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20160518
+// Created by iWeb 3.0.4 local-build-20160729
 
 setTransparentGifURL('../../../../Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
@@ -6,6 +6,6 @@ function hostedOnDM()
 {return false;}
 function onPageLoad()
 {dynamicallyPopulate();loadMozillaCSS('20_Orthodontic_Residency_files/20_Orthodontic_ResidencyMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');adjustLineHeightIfTooBig('id8');adjustFontSizeIfTooBig('id8');fixAllIEPNGs('../../../../Media/transparent.gif');Widget.onload();fixupAllIEPNGBGs();BlogFixupPreviousNext();applyEffects()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');adjustLineHeightIfTooBig('id8');adjustFontSizeIfTooBig('id8');fixAllIEPNGs('../../../../Media/transparent.gif');Widget.onload();BlogFixupPreviousNext();applyEffects()}
 function onPageUnload()
 {Widget.onunload();}
