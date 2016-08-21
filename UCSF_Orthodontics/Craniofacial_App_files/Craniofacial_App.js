d97aec3c930ff7d18734f0ccbfc273606ef1173a
Kjeld Aamodt <Kjeld.aamodt@gmail.com>
2016-08-21 15:17:57 -0700

    updated cranio

---
+++ UCSF_Orthodontics/Craniofacial_App_files/Craniofacial_App.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20160821
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Craniofacial_App_files/Craniofacial_AppMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');Widget.onload();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id3');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
