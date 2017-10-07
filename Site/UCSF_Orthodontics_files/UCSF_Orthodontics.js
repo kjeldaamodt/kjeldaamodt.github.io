ed2da2f58b1b0c0bd87d18bdd557eb138a8d857e
Kjeld Aamodt <Kjeld.aamodt@gmail.com>
2017-10-07 11:25:13 -0700

    Take Down, Add Redirect
    Taking down aamodt’s ortho site, adding redirect. Per Direction of
    Sunil Kapila on 10-7-17 at 11am https://www.ucsfdentalcenter.org/clinics

---
+++ Site/UCSF_Orthodontics_files/UCSF_Orthodontics.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20171007
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('UCSF_Orthodontics_files/UCSF_OrthodonticsMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
